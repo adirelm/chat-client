02db715eed771e517156df15f03fe42a589068e4
adirelmakais <104345864+AdirElmakais@users.noreply.github.com>
2023-10-30 18:46:53 +0200

    WIP

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const consumerToken =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJmYW1pbHkiOiJkZTNmYTY5OS1hYWVkLTQxYmUtYjI3Zi0xMzhkYjU0Mzg4MWIiLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoxLCJzdWJUeXBlIjoiVXNlciIsInNvdXJjZUlkIjoyLCJzb3VyY2VUeXBlIjoiRW1haWxPdHBDcmVkZW50aWFsU2V0IiwiaXNUZXN0QWNjb3VudCI6ZmFsc2UsInV1aWQiOiIxNTM1YmYyZi1mNDAwLTQ0NjEtYTE5Yy1lZTI4ZDMzYTJiZTUiLCJpYXQiOjE2OTg0ODkwOTksImV4cCI6MTAwMTY5ODQ4OTA5OCwiaXNzIjoiYnJlbmRvbiJ9.Oc1nVxuti8KFlBV9gkbD9mqr8xpkcNiy6qJ2c3j0mN-YZkcao0o4rORPOoFztmaO7hEbay-yWq8_C5KecO9dPSwosnuNifhsxb0SmbrEJv6UhsI_7vbHq5Er-0UCOn-zthITv4GVZQTo-xFm_zcXst3I1e_ap956sInUI0SV6vO5UmnQTFxHXm2eydU6tv4CSWIHAIiT7hi9M7Fs15oi9cpxrl-66cXACUyL9gnV-JSC5KUPMdYvCt3Ayd8GApFnQisM5VFL6nc38O47dzVkerPDtLJiFE0Lr77inTPPbtejUd3PuO7goQgv0u19YOfP_Jf2hhFFSyeKAid18Ob0UQ";
 
 const expertToken =
-  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJmYW1pbHkiOiJkN2Y0MmMyYy04YWExLTQzYjQtYTQ4OS1iMmYyYTUzZTg3MTMiLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoxLCJzdWJUeXBlIjoiRXhwZXJ0Iiwic291cmNlSWQiOjEsInNvdXJjZVR5cGUiOiJFbWFpbFBhc3N3b3JkQ3JlZGVudGlhbFNldCIsImlzVGVzdEFjY291bnQiOmZhbHNlLCJ1dWlkIjoiNjllMmUyM2EtMWUxNi00Njc1LTlkZWMtNDY5ODViNjA4ODZhIiwiaWF0IjoxNjk4NDg5MTEzLCJleHAiOjEwMDE2OTg0ODkxMTIsImlzcyI6ImJyZW5kb24ifQ.Uk-XLAkXDp3u1rePVt9OYOKTe5byDAJp8_h2cqIH1BL-7AZ9qDR78RNgbVy3QKqPdXjvhNynQrq8wY1CPMVfelf3E8-9jgyYoOb14RaB2RCHrNvHjf1RBWVEBqR9rHMZzTLnK4VCYZ8-RJBOpnQvLtAnrVpx-kdtbT8025v-qkpoeVJZilOpe3vPKEpBA2uRJc2bKgBhH0W4WkskaWkQC03rt3RG5CxepzyrXPKdfcS0rCqc3-Vpb7LKURzovrR0qrXG9jN9XgvBpL3B_eWS02l-zewT9ettiwvV1ZGmKir5aLOUIDWQHVbXNO1qrFx_aJ3g5-Oj_Z0J7T_Ic5LF8w";
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJmYW1pbHkiOiI4OTI1MGE3MS0xMGM1LTRlYmYtYjA3NC00MDY4NGNlYjljYTIiLCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoxLCJzdWJUeXBlIjoiRXhwZXJ0Iiwic291cmNlSWQiOjEsInNvdXJjZVR5cGUiOiJFbWFpbFBhc3N3b3JkQ3JlZGVudGlhbFNldCIsImlzVGVzdEFjY291bnQiOmZhbHNlLCJ1dWlkIjoiNzk0N2QzMjMtMTc0YS00MDAyLTkzMTctNmI0Zjc3OTAzNGZjIiwiaWF0IjoxNjk4NjA0NDI2LCJleHAiOjEwMDE2OTg2MDQ0MjUsImlzcyI6ImJyZW5kb24ifQ.U6R9p9qXsR5X5kA5fkUxtNd8XGRZoBkMXIIkOJ9FIwYCxlYB_ZehMNFpPg02lNMgBUYBUFNHp_kPXmsxSNC1OtNPLChSrhbNnLkr1u2ccD2dEKgn-SpMlliAsE_ngB5kwCCdpZc-RojqkVZ_2Gg2Y4e4Lyx9OZc_HkCjXV6QRrMkyldOUMW8AFOD5ABhaWz_i-qCdii1e-3r_QA410_Q-AAqeF-R7_VMxCdr_8B7mwSlZ2aVIgFR5uh1OwwvFuQQlZfefa7wrJWJ5yQXxKJL-9M3j7_uNBAzHTm268_Cf5Z6Dq0PVXJ1icMIosBWHUyNRp6dyZ0T-H09rYx_sHphOA";
 
 const choice = window.prompt(
   "Choose a token: Type 'consumer' for consumer or 'expert' for expert"
@@ -16,8 +16,11 @@ const choice = window.prompt(
 let token = choice === "consumer" ? consumerToken : expertToken;
 
 const ENDPOINT =
-  process.env.REACT_APP_SERVER_BASE_URL || "http://localhost:3000";
-const socketInit = io(ENDPOINT, { transports: ["websocket"], auth: { token } });
+  process.env.REACT_APP_SERVER_BASE_URL || "https://cal-server-staging-85674c7c21e8.herokuapp.com/";
+const socketInit = io(ENDPOINT, {
+  transports: ["websocket"],
+  auth: { token },
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,7 +42,6 @@ const useStyles = makeStyles((theme) => ({
 export default function App() {
   const [socket] = useState(socketInit);
   const classes = useStyles();
-  console.log("here");
   useEffect(() => {
     socket.on("connect", () => {
       console.log(`Client connected successfully`, socket);
